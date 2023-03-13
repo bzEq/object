04196873864c1d7dee730f8dc6557cf276925282
Kai Luo <gluokai@gmail.com>
2023-03-13 13:17:17 +0800

    Outline of writing XCOFF object file

--- src/write/xcoff.rs
+++ src/write/xcoff.rs
@@ -13,12 +13,36 @@ struct XcoffObjectWriter<'a> {
     buffer: &'a mut dyn WritableBuffer,
 
     is_64: bool,
-    num_sections: usize,
     symbol_table_offset: usize,
     num_symbol_table_entry: usize,
     string_table_offset: usize,
     string_table: StringTable<'a>,
     string_table_data: Vec<u8>,
+    symbol_section: HashMap<SymbolId, XcoffSectionIndex>,
+    section_offset: HashMap<XcoffSectionIndex, usize>,
+    csect_group: HashMap<XcoffSectionIndex, Vec<SectionId>>,
+    csect_offset: HashMap<SectionId, usize>,
+    // C_INFO symbol's offset in .info section.
+    info_symbol_offset: HashMap<SymbolId, usize>,
+    filenames: Vec<SymbolId>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum XcoffSectionIndex {
+    Text = 1,
+    Data,
+    Info,
+}
+
+impl XcoffSectionIndex {
+    fn from_i32(i: i32) -> Self {
+        match i {
+            1 => XcoffSectionIndex::Text,
+            2 => XcoffSectionIndex::Data,
+            3 => XcoffSectionIndex::Info,
+            _ => panic!("Out of bound index"),
+        }
+    }
 }
 
 impl<'a> XcoffObjectWriter<'a> {
@@ -32,11 +56,105 @@ impl<'a> XcoffObjectWriter<'a> {
     }
 
     fn layout(&mut self) -> Result<()> {
-        for (_, section) in self.object.sections.iter().enumerate() {}
+        // Add .text and .data XCOFF sections to make AIX toolchain happy.
+        // Add .info XCOFF section to store rust metadata.
+        self.csect_group.insert(XcoffSectionIndex::Text, Vec::new());
+        self.csect_group.insert(XcoffSectionIndex::Data, Vec::new());
+        self.csect_group.insert(XcoffSectionIndex::Info, Vec::new());
+        let num_sections = self.csect_group.len();
+        // FIXME: Relocation is not supported yet.
+        let mut object_file_offset = 0;
+        let (header_size, section_header_size, _, symbol_size) = if self.is_64 {
+            (
+                mem::size_of::<xcoff::FileHeader64>(),
+                mem::size_of::<xcoff::SectionHeader64>(),
+                mem::size_of::<xcoff::Rel64>(),
+                mem::size_of::<xcoff::Symbol64>(),
+            )
+        } else {
+            (
+                mem::size_of::<xcoff::FileHeader32>(),
+                mem::size_of::<xcoff::SectionHeader32>(),
+                mem::size_of::<xcoff::Rel32>(),
+                mem::size_of::<xcoff::Symbol32>(),
+            )
+        };
+        object_file_offset += header_size;
+        // FIXME: Support auxiliary file header for XCOFF32.
+        object_file_offset += section_header_size * num_sections;
+        // For every section in object.sections, we treat it as a csect(control section).
+        let default_section_align = 4;
+        for (id, section) in self.object.sections.iter().enumerate() {
+            self.string_table.add(&section.name);
+            // Currently, we only support SetionKind::OtherString which is mapped to
+            // XCOFF's .info section, i.e., comment section.
+            match section.kind {
+                SectionKind::OtherString => {
+                    self.csect_group
+                        .get_mut(&XcoffSectionIndex::Info)
+                        .unwrap()
+                        .push(SectionId(id));
+                }
+                _ => {}
+            }
+        }
+        for i in (XcoffSectionIndex::Text as i32)..(XcoffSectionIndex::Info as i32 + 1) {
+            let csect_group = &self
+                .csect_group
+                .get(&(XcoffSectionIndex::from_i32(i)))
+                .unwrap();
+            for id in csect_group.iter() {
+                let section = self.object.section(*id);
+                let section_align = std::cmp::max(section.align, default_section_align);
+                object_file_offset = align(object_file_offset, section_align as usize);
+                self.csect_offset.insert(*id, object_file_offset);
+                object_file_offset += section.size as usize;
+            }
+        }
+        // FIXME: Currently we only support C_INFO symbols which is a reference to
+        // data stored in XCOFF's .info section. For these C_INFO symbols, we currently
+        // set their visibility to SYM_V_INTERNAL.
+        let mut info_symbol_offset = 0;
         for (_, symbol) in self.object.symbols.iter().enumerate() {
             self.string_table.add(&symbol.name);
+            if symbol.kind == SymbolKind::File {
+                let sym_id = self.object.symbol_id(&symbol.name).unwrap();
+                self.filenames.push(sym_id);
+                continue;
+            }
+            match symbol.section {
+                SymbolSection::Section(id) => {
+                    // We currently only handle symbols defined in .info section.
+                    // FIXME: For other symbols, we may map them to XCOFF's csect and create
+                    // auxiliar entries for them.
+                    if self
+                        .csect_group
+                        .get(&XcoffSectionIndex::Info)
+                        .unwrap()
+                        .contains(&id)
+                    {
+                        if let Some(sym_id) = self.object.symbol_id(&symbol.name) {
+                            self.info_symbol_offset.insert(sym_id, info_symbol_offset);
+                            // For C_INFO symbol's data, it's prefixed with a word indicating
+                            // the data's length.
+                            info_symbol_offset += 4 + symbol.size as usize;
+                            // C_INFO symbol doesn't have auxiliary entry.
+                            self.num_symbol_table_entry += 1;
+                            self.symbol_section.insert(sym_id, XcoffSectionIndex::Info);
+                        }
+                    }
+                }
+                _ => {}
+            }
         }
+        // If no file symbol exists, we add one to make AIX toolchain happy.
+        if self.filenames.is_empty() {
+            self.num_symbol_table_entry += 1;
+        }
+        self.symbol_table_offset = object_file_offset;
+        object_file_offset += symbol_size * self.num_symbol_table_entry;
         self.finalize_string_table();
+        self.string_table_offset = object_file_offset;
         return Ok(());
     }
 
@@ -44,7 +162,7 @@ impl<'a> XcoffObjectWriter<'a> {
         if self.is_64 {
             self.buffer.write(&xcoff::FileHeader64 {
                 f_magic: U16::new(BE, xcoff::MAGIC_64),
-                f_nscns: U16::new(BE, self.num_sections as u16),
+                f_nscns: U16::new(BE, self.csect_group.len() as u16),
                 f_timdat: U32::new(BE, 0),
                 f_symptr: U64::new(BE, self.symbol_table_offset as u64),
                 f_nsyms: U32::new(BE, self.num_symbol_table_entry as u32),
@@ -57,7 +175,7 @@ impl<'a> XcoffObjectWriter<'a> {
         } else {
             self.buffer.write(&xcoff::FileHeader32 {
                 f_magic: U16::new(BE, xcoff::MAGIC_32),
-                f_nscns: U16::new(BE, self.num_sections as u16),
+                f_nscns: U16::new(BE, self.csect_group.len() as u16),
                 f_timdat: U32::new(BE, 0),
                 f_symptr: U32::new(BE, self.symbol_table_offset as u32),
                 f_nsyms: U32::new(BE, self.num_symbol_table_entry as u32),
@@ -129,12 +247,17 @@ impl<'a> Object<'a> {
             object: self,
             buffer: buffer,
             is_64: is_64(self),
-            num_sections: 0,
             num_symbol_table_entry: 0,
             symbol_table_offset: 0,
             string_table_offset: 0,
             string_table: StringTable::default(),
             string_table_data: Vec::new(),
+            csect_group: HashMap::new(),
+            csect_offset: HashMap::new(),
+            info_symbol_offset: HashMap::new(),
+            section_offset: HashMap::new(),
+            symbol_section: HashMap::new(),
+            filenames: Vec::new(),
         };
         return xcoff.write();
     }
